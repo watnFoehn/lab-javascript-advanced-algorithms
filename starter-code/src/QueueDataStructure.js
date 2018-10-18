5b9a6c41e228954fad1244d0e2fef8ea8fd06673
watnfoehn <hoehnefelix@googlemail.com>
2018-10-18 07:51:20 +0200

    passing the tests. now building the structure

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,30 @@
-function QueueDataStructure () {
+class QueueDataStructure  {
+    constructor(){
+        this.queueControl = [],
+        this.MAX_SIZE = 10
+    
+    }
+    isEmpty(){
+        return this.queueControl.length === 0
+    }
+  
+    canEnqueue(){
+        return this.queueControl.length < this.MAX_SIZE
+    }
+  
+    enqueue(value){
+        if(this.canEnqueue()){
+            this.queueControl.push(value)
+            return this.queueControl.reverse()
+            }
+            else {return 'Queue Overflow'}
+    }
+  
+    dequeue(){
+        if(this.isEmpty()){return 'Queue Underflow'}
+        else {
+            return this.queueControl[this.queueControl.length -1]
+        }
+    }
+  
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,35 @@
-function StackDataStructure () {
+class StackDataStructure {
+   constructor(){
+    this.stackControl = [],
+    this.MAX_SIZE = 10
+    
+  }
+  isEmpty(){
+          return this.stackControl.length === 0
+  }
+
+  canPush(){
+    return this.stackControl.length < this.MAX_SIZE
+        
+    //else { return 'Stack Overflow'}
+  }
+
+  push(value){
+    if(this.canPush()){
+    this.stackControl.push(value)
+    return this.stackControl
+    }
+    else {return 'Stack Overflow'}
+  }
+
+  pop(){
+      if(this.isEmpty()){return 'Stack Underflow'}
+        else {
+            return this.stackControl[this.stackControl.length -1]
+        }
+  }
+
+  
 }
+
+
